9f7f8a030fbe9cbf2a95364fafc316ba9fc43616
Prezlyata Yuriy <Prezlyata Yuriy>
2018-10-29 11:42:33 +0200

    refactor function

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 (function() {
     'use strict';
 
-    const productsInCart = (function(a,b) {
-        this.a = a;
-        this.b = b;
-        return function (a,b) {
-            return a + b
-        };
-    })();
+    const productsInCart = (a,b) => {
+        return a + b
+    };
 
     module.exports = productsInCart;
 })(this);
