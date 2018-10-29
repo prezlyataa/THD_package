108795bb13ccf6a466025a61bdbed7ada78b4c74
Prezlyata Yuriy <Prezlyata Yuriy>
2018-10-29 11:23:44 +0200

    pure function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-(function(global) {
+(function() {
     'use strict';
 
     const products_in_cart = (function() {
@@ -7,14 +7,6 @@
         };
     })();
 
-    if (typeof define === 'function') {
-        define(function () {
-            return products_in_cart;
-        });
-    } else if (typeof exports === 'object') {
-        module.exports = products_in_cart;
-    } else {
-        global.products_in_cart = products_in_cart;
-    }
+    module.exports = products_in_cart;
 })(this);
 
