4d8d9adb8e4e7a6649ec547c4ea01c7ff5eb52b7
Prezlyata Yuriy <Prezlyata Yuriy>
2018-10-29 12:46:47 +0200

    created class CartProducts

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 (function() {
     'use strict';
 
-    const productsInCart = (a,b) => {
-        return a + b
-    };
+    class CartProducts {
+        constructor() {
+            this.storage = window.localStorage;
+        }
 
-    module.exports = productsInCart;
+        addProduct(id, pageType) {
+            if(!this.storage.hasOwnProperty('productsInCart')) {
+                this.storage.setItem('productsInCart', JSON.stringify({}));
+            }
+            let productsInCart = JSON.parse(this.storage.getItem('productsInCart'));
+            if(!productsInCart.hasOwnProperty(id)) {
+                productsInCart[id] = { pip: 0, conf: 0 };
+            } else {
+                if(pageType === 'pip') {
+                    productsInCart[id].pip++;
+                }
+                if(pageType === 'conf') {
+                    productsInCart[id].conf++;
+                }
+            }
+            this.storage.setItem('productsInCart', JSON.stringify(productsInCart));
+        }
+    }
+
+    module.exports = CartProducts;
 })(this);
 
